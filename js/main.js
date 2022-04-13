700c3676b98842618e950be6fd09ae645268ed12
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-04-13 19:01:46 +0500

    Finished footer

--- js/main.js
+++ js/main.js
@@ -507,3 +507,58 @@ elExternal.addEventListener("click", () => {
   elExternal.style.fontSize = "15px";
   elExternal.innerHTML = "Nusxa olindi";
 });
+
+// Desc buttons
+
+let elDescBtn = document.querySelector(".desc__summary");
+let elDescSpec = document.querySelector(".desc__spec");
+let elColor = document.querySelector(".desc__color");
+let eldescSummaryInfo = document.querySelector(".desc__summary-info");
+let elDescSpecification = document.querySelector(".desc__specifications");
+
+elDescSpec.addEventListener("click", () => {
+  elColor.style.transform = "translateX(100%)";
+  eldescSummaryInfo.classList.remove("show-it");
+  elDescSpecification.classList.add("show-it");
+});
+
+elDescBtn.addEventListener("click", () => {
+  elColor.style.transform = "translateX(0)";
+  eldescSummaryInfo.classList.add("show-it");
+  elDescSpecification.classList.remove("show-it");
+});
+
+// Bottom slider
+
+let elSimilarList = document.querySelector(".similar__list");
+let elRight = document.querySelector(".right");
+let elLeft = document.querySelector(".left");
+let elCheck = 0;
+
+elRight.addEventListener("click", () => {
+  elCheck++;
+
+  carouselIt();
+});
+
+elLeft.addEventListener("click", () => {
+  elCheck--;
+
+  carouselIt();
+});
+
+function carouselIt(){
+  if(elCheck == 0){
+    elLeft.style.display = "none";
+  } else if(elCheck > 0){
+    elLeft.style.display = "block";
+  }
+
+  if(elCheck == 7){
+    elRight.style.display = "none";
+  } else{
+    elRight.style.display = "block";
+  }
+
+  elSimilarList.style.transform = `translate(${-elCheck * 33.3}%)`;
+}
